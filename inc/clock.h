9370e20ad0e45759fe16ff151ab1334eb255d368
klaus-strassner <strassnerk02@gmail.com>
2025-12-17 16:30:31 +0100

    feat(clock): implement dynamic clock initialization logic - configuring PLL - enabling over drive mode if needed - setting flash state - setting AHB and APB prescalers

--- inc/clock.h
+++ inc/clock.h
@@ -1,6 +1,98 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
+#include "stm32f730xx.h"
+
+
+#define CLOCK_VCO_OUT               384
+#define CLOCK_SYS                   192
+#define CLOCK_AHB                   192
+#define CLOCK_APB1                  48
+#define CLOCK_APB2                  96
+
+#define CLOCK_HSE                   24
+#define CLOCK_VCO_IN                2
+#define CLOCK_USB                   48
+
+// define PLL config values
+#define CLOCK_PLLM                  CLOCK_HSE/CLOCK_VCO_IN
+#define CLOCK_PLLN                  CLOCK_VCO_OUT/CLOCK_VCO_IN
+#define CLOCK_PLLP                  (CLOCK_VCO_OUT/CLOCK_SYS)/2-1
+#define CLOCK_PLLQ                  CLOCK_VCO_OUT/CLOCK_USB
+
+// define AHB prescaler
+#if(CLOCK_SYS/CLOCK_AHB == 1)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV1
+#elif(CLOCK_SYS/CLOCK_AHB == 2)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV2
+#elif(CLOCK_SYS/CLOCK_AHB == 4)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV4
+#elif(CLOCK_SYS/CLOCK_AHB == 8)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV8
+#elif(CLOCK_SYS/CLOCK_AHB == 16)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV16
+#elif(CLOCK_SYS/CLOCK_AHB == 64)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV64
+#elif(CLOCK_SYS/CLOCK_AHB == 128)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV128
+#elif(CLOCK_SYS/CLOCK_AHB == 256)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV256
+#elif(CLOCK_SYS/CLOCK_AHB == 512)
+    #define CLOCK_HPRE              RCC_CFGR_HPRE_DIV512
+#else
+    #error "invalid ratio"
+#endif
+
+// define APB1 prescaler
+#if(CLOCK_SYS/CLOCK_APB1 == 1)
+    #define CLOCK_PPRE1              RCC_CFGR_PPRE1_DIV1
+#elif(CLOCK_SYS/CLOCK_APB1 == 2)
+    #define CLOCK_PPRE1              RCC_CFGR_PPRE1_DIV2
+#elif(CLOCK_SYS/CLOCK_APB1 == 4)
+    #define CLOCK_PPRE1              RCC_CFGR_PPRE1_DIV4
+#elif(CLOCK_SYS/CLOCK_APB1 == 8)
+    #define CLOCK_PPRE1              RCC_CFGR_PPRE1_DIV8
+#elif(CLOCK_SYS/CLOCK_APB1 == 16)
+    #define CLOCK_PPRE1              RCC_CFGR_PPRE1_DIV16
+#else
+    #error "invalid ratio"
+#endif
+
+// define APB2 prescaler
+#if(CLOCK_SYS/CLOCK_APB2 == 1)
+    #define CLOCK_PPRE2              RCC_CFGR_PPRE2_DIV1
+#elif(CLOCK_SYS/CLOCK_APB2 == 2)
+    #define CLOCK_PPRE2              RCC_CFGR_PPRE2_DIV2
+#elif(CLOCK_SYS/CLOCK_APB2 == 4)
+    #define CLOCK_PPRE2              RCC_CFGR_PPRE2_DIV4
+#elif(CLOCK_SYS/CLOCK_APB2 == 8)
+    #define CLOCK_PPRE2              RCC_CFGR_PPRE2_DIV8
+#elif(CLOCK_SYS/CLOCK_APB2 == 16)
+    #define CLOCK_PPRE2              RCC_CFGR_PPRE2_DIV16
+#else
+    #error "invalid ratio"
+#endif
+
+// define wait state for 3.3V
+#if (CLOCK_SYS <= 30)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_0WS
+#elif (CLOCK_SYS <= 60)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_1WS
+#elif (CLOCK_SYS <= 90)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_2WS
+#elif (CLOCK_SYS <= 120)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_3WS
+#elif (CLOCK_SYS <= 150)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_4WS
+#elif (CLOCK_SYS <= 180)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_5WS
+#elif (CLOCK_SYS <= 210)
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_6WS
+#else
+    #define CLOCK_FLASH_WS    FLASH_ACR_LATENCY_7WS
+#endif
+
+
 void clock_init(void);
 
 #endif
--- src/clock.c
+++ src/clock.c
@@ -1,6 +1,55 @@
-#include "clock.h"
-#include "stm32f7xx.h"
+    #include "clock.h"
+    #include "stm32f730xx.h"
 
-void clock_init(void) {
+    void clock_init(void) {
+        // turn on HSE
+        RCC->CR |= RCC_CR_HSEON;
+        while(!(RCC->CR & RCC_CR_HSERDY));
 
-}
+        // turn off PLL
+        RCC->CR &= ~RCC_CR_PLLON;
+        while(RCC->CR & RCC_CR_PLLRDY);
+
+        // configure PLL
+        MODIFY_REG(RCC->PLLCFGR,
+            RCC_PLLCFGR_PLLM | RCC_PLLCFGR_PLLN | RCC_PLLCFGR_PLLP | RCC_PLLCFGR_PLLQ | RCC_PLLCFGR_PLLSRC,
+            _VAL2FLD(RCC_PLLCFGR_PLLM, CLOCK_PLLM) | // set HSE divider for PLL
+            _VAL2FLD(RCC_PLLCFGR_PLLN, CLOCK_PLLN) | // set PLL multiplier for VCO
+            _VAL2FLD(RCC_PLLCFGR_PLLP, CLOCK_PLLP) | // set PLL divider for sysclock
+            _VAL2FLD(RCC_PLLCFGR_PLLQ, CLOCK_PLLQ) | // set PLL divider for USB
+            RCC_PLLCFGR_PLLSRC_HSE); // set PLL source to HSE
+
+        // turn on PLL
+        RCC->CR |= RCC_CR_PLLON;
+        while(!(RCC->CR & RCC_CR_PLLRDY));
+
+        // enable overdrive mode
+        PWR->CR1 |= PWR_CR1_ODEN;
+        while(!(PWR->CSR1 & PWR_CSR1_ODRDY));
+        PWR->CR1 |= PWR_CR1_ODSWEN;
+        while(!(PWR->CSR1 & PWR_CSR1_ODSWRDY));
+
+        // set flash memory wait states
+        MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY, CLOCK_FLASH_WS);
+
+        // set APB prescalers
+        MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, CLOCK_PPRE1);
+        MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, CLOCK_PPRE2);
+
+        // set AHB prescaler
+        MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, CLOCK_HPRE);
+
+        // wait for PLL lock
+        while(!(RCC->CR & RCC_CR_PLLRDY));
+
+        // switch to PLL used as sysclock
+        RCC->CFGR |= RCC_CFGR_SW_PLL;
+        while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+        // enable USB OTG HS PHY Controller
+        RCC->APB2ENR |= RCC_APB2ENR_OTGPHYCEN;
+
+        // turn off HSI
+        RCC->CR &= ~RCC_CR_HSION;
+        while((RCC->CR & RCC_CR_HSIRDY));
+    }
